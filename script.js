14ecc4c5eddda8e063d3c326e2db28fa759ef46a
Dniil-Pavlicenko2002 <pavlichenko.02@internet.ru>
2023-08-19 11:15:57 +0300

    final

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ const btn = document.querySelector('.btn')
 const result = document.querySelector('.result')
 
 btn.addEventListener('click', () => {
+    function validateEmail(email) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
     if (email.value.indexOf('@') != -1 && password.value === '12345678' && password.value === password2.value && password.value.length >= 8) {
         result.innerHTML = 'Форма отправлена'
         result.classList.add('green')
     } else if (email.value.length === 0) {
         result.innerHTML = 'Поле email пустое'
         result.classList.add('red')
-    } else if (!(email.value.indexOf('@') != -1)) {
-        result.innerHTML = 'Нет символа @ в Email'
-        console.log(email.value.length)
+    } else if (!(validateEmail(email.value))) {
+        result.innerHTML = 'Недопустимое значение Email'
         result.classList.add('red')
     } else if (password.value.length === 0) {
         result.innerHTML = 'Поле пароля пустое'
